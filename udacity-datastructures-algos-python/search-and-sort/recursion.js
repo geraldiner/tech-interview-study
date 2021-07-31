8328494550c75df9ed41d07729037170f23d1e0b
Geraldine <hello@geraldiner.com>
2021-07-31 14:14:01 +0000

    complete recursion (fibonacci sequence) practice problem

--- udacity-datastructures-algos-python/search-and-sort/recursion.js
+++ udacity-datastructures-algos-python/search-and-sort/recursion.js
@@ -3,8 +3,10 @@
 // Your code should have the same input/output as the 
 // iterative code in the instructions."""
 
-function get_fib(position) {
-	return -1
+function get_fib(pos) {
+	if (pos < 0) { return -1 }
+	if (pos === 0 || pos === 1) { return pos }
+	return get_fib(pos - 1) + get_fib(pos - 2);
 }
 
 // # Test cases
