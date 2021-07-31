2282f98507d7f6ec9940a3832c0ae33de2ece24d
Geraldine <hello@geraldiner.com>
2021-07-31 13:56:41 +0000

    set up recursion (fibonacci sequence) practice problem

---
+++ udacity-datastructures-algos-python/search-and-sort/recursion.js
@@ -0,0 +1,13 @@
+// """Implement a function recursively to get the desired
+// Fibonacci sequence value.
+// Your code should have the same input/output as the 
+// iterative code in the instructions."""
+
+function get_fib(position) {
+	return -1
+}
+
+// # Test cases
+console.log(get_fib(9))
+console.log(get_fib(11))
+console.log(get_fib(0))
