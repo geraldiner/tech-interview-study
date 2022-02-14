7904227f22eced2d9355b91b742112e1e4a3dd6c
Geraldine <hello@geraldiner.com>
2022-02-14 18:23:44 -0500

    add: dsa-practice- structy- binary-tree- treeIncludes iterative DFS, iterative BFS, recursive DFS solutions

--- dsa-practice/structy/2-binary-tree/treeIncludes/index.js
+++ dsa-practice/structy/2-binary-tree/treeIncludes/index.js
@@ -9,8 +9,40 @@ class Node {
 // @param: root - reference to the root of a binary tree
 // @param: target - value to be found in the tree
 // @return: bool - whether or not the target is found in root
+
+// iterative DFS
+/* const treeIncludes = (root, target) => {
+	if (root === null) return false;
+	const stack = [root];
+	while (stack.length > 0) {
+		const curr = stack.pop();
+		if (curr.val === target) return true;
+
+		if (curr.right) stack.push(curr.right);
+		if (curr.left) stack.push(curr.left);
+	}
+	return false;
+}; */
+
+// iterative BFS
+/* const treeIncludes = (root, target) => {
+	if (root === null) return false;
+	const queue = [root];
+	while (queue.length > 0) {
+		const curr = queue.shift();
+		if (curr.val === target) return true;
+
+		if (curr.right) queue.push(curr.right);
+		if (curr.left) queue.push(curr.left);
+	}
+	return false;
+}; */
+
+// recursive DFS
 const treeIncludes = (root, target) => {
-	// todo
+	if (root === null) return false;
+	if (root.val === target) return true;
+	return treeIncludes(root.left, target) || treeIncludes(root.right, target);
 };
 
 const a = new Node("a");
@@ -32,6 +64,6 @@ c.right = f;
 //  / \     \
 // d   e     f
 
-treeIncludes(a, "e"); // -> true
-treeIncludes(a, "n"); // -> false
-treeIncludes(null, "b"); // -> false
+console.log(treeIncludes(a, "e")); // -> true
+console.log(treeIncludes(a, "n")); // -> false
+console.log(treeIncludes(null, "b")); // -> false
